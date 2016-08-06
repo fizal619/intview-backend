6b9328dbc2418a7d8d586271fb963a66cd9a73ae
Fizal Sarif <fizal.sarif@gmail.com>
2016-08-06 18:33:18 -0400

    got devise working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :user, only: []
+
+  namespace :v1, defaults: { format: :json } do
+  	resources :users, only: [:create]
+    resource :login, only: [:create], controller: :sessions
+  end
 end
