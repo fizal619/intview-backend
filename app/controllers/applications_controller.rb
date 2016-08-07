04069e8fef7be8ab505c41fdf0b1557eff6a2194
Fizal Sarif <fizal.sarif@gmail.com>
2016-08-07 16:14:11 -0400

    create application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,5 +7,17 @@ class ApplicationsController < ApplicationController
     render json: user.applications
   end
 
+  def create
+    #find the user who's token we got, then save an application.
+    id = request.headers['Authorization'].split(':')[0]
+    user = User.find(id)
 
+    Application.create({
+      title: params['title'],
+      questions: params['questions'],
+      user: user
+      })
+
+    render json: {status: 'success'}
+  end
 end
