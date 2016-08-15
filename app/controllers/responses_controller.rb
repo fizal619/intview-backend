39d895ab258d99a925f5088c2cef707452261abd
Fizal Sarif <fizal.sarif@gmail.com>
2016-08-14 21:23:47 -0400

    updated word limit

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -22,7 +22,7 @@ class ResponsesController < ApplicationController
 
     countContent = content.split(' ')
 
-    if (countContent.size < 1000 || nil)
+    if (countContent.size < 550 || nil)
       render json: {status: 'null'}
     else
       response = Excon.post("https://gateway.watsonplatform.net/personality-insights/api/v2/profile",
